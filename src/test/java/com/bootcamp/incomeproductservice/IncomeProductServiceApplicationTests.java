f5923a83628704700a9f527a28fcacc2205b7509
HaroldQuispeC <hquispco@everis.com>
2022-06-06 15:01:44 -0500

    first commit ms-income-product-service

---
+++ src/test/java/com/bootcamp/incomeproductservice/IncomeProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.incomeproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
